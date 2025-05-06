fd64c64e481ce1d71c1834b95beefae8d5145e85
Taylor Spaulding <tspaulding@esassoc.com>
2025-05-06 07:49:54 -0700

    Variable Parameter Code Rewrite

---
+++ 02_Code/08_Variable_Parameter_Consumption.R
@@ -0,0 +1,115 @@
+################################################################################
+#
+# 4. Variable Parameter Calculations
+#
+################################################################################
+
+# Total Basic with Direct Accumulation, p < 1
+
+# for each predator, calculate the amount of food they would have eaten
+#   over each day, accounting for temperature, but ignoring
+#   predator growth
+#   repeat NPERM amount of times
+
+# because gam predict only gives the error of the fit, 
+#    we expand the error to include residuals/generate prediction interval
+library(tidyverse)
+library(lubridate)
+library(foreach)
+library(parallel)
+library(doSNOW)
+library(progress)
+set.seed(123)
+
+# Bring in the Data
+LWdataA <- readRDS(file.path("01_Data","Output","LWdataA.rds"))
+dietfVCHN <- readRDS(file.path("01_Data","Input","diet_fraction_variable_analysis.rds"))
+TeT <- readRDS(file.path("01_Data","Input","temps.rds")) %>%
+  group_by(WaterYear, Date) %>%
+  summarize(Temp = mean(Value, na.rm = TRUE))
+
+#Bring in the consumption functions
+g1 <- readRDS(file.path("01_Data","Input","g1.rds"))
+g2 <- readRDS(file.path("01_Data","Input","g2.rds"))
+g3 <- readRDS(file.path("01_Data","Input","g3.rds"))
+gs <- list(g1, g2, g3, g3, g3, g3, g3, g3, g3, g3)
+
+pred_cmax_lu <- readRDS(file.path("01_Data","Input","pred_cmax_lu.rds"))
+r_sq_lu <- readRDS(file.path("01_Data","Input","r_sq_lu.rds"))
+ref_cmax_lu <- readRDS(file.path("01_Data","Input","ref_cmax_lu.rds"))
+con_allo_lu <- readRDS(file.path("01_Data","Input","ConAlloLU.rds"))
+
+# set up parameters based on age classes
+pvals <- c(rep(0.69, 2), rep(0.73, 1), rep(0.68, 1), rep(0.62, 6))
+
+NPERM <- 100
+
+LWdataA <- LWdataA %>%
+  mutate(wy = esaRmisc::water_year(date))
+
+# Setup the parallel processing
+# Cores
+n_cores <- detectCores()
+cl <- makeCluster(n_cores - 2)
+doSNOW::registerDoSNOW(cl)
+
+TCHNconsmatVA_plt1 <-list()
+for(y in unique(LWdataA$wy)){
+  LWdataA_y <- LWdataA[LWdataA[["wy"]] == y,]
+  TeT_y <- TeT[TeT[["WaterYear"]] == y,]
+  
+  TE <- TeT_y$Temp
+  print(y)
+  
+  # Progress Bar
+  iter = nrow(LWdataA_y)
+  pb <- progress_bar$new(
+    format = "[:bar] :elapsed | eta: :eta",
+    total = iter, 
+    width = 60)
+  
+  # allowing progress bar to be used in foreach -----------------------------
+  progress <- function(n){
+    pb$tick()
+  } 
+  
+  opts <- list(progress = progress)
+  
+  fish_list <- list()
+  foreach(f = 1:nrow(LWdataA_y), .options.snow = opts) %dopar% {
+    LWdata_f <- LWdataA_y[f,] |>
+      dplyr::mutate(WW = list(round(rnorm(NPERM,  LWdataA_y$fit[i], 
+                                   (LWdataA_y$fit[i] - LWdataA_y$lwr[i]) / 1.96),2))) |>
+      dplyr::bind_cols(TeT_y) |>
+      tidyr::unnest(WW) |>
+      dplyr::mutate(WW = round(WW,3)) |>
+      dplyr::rename("TE" = Temp) |>
+      dplyr::left_join(pred_cmax_lu) |>
+      dplyr::mutate(cmax_sds = cmax_se.fit/r_sq_lu[age][[1]]) |>
+      dplyr::left_join(ref_cmax_lu) |>
+      dplyr::left_join(con_allo_lu) |>
+      dplyr::mutate(TCHN_cons_plt1 = rnorm(NPERM, cmax_mean, cmax_sds) / 
+                      rnorm(NPERM, ref_cmax_mean,ref_cmax_sds) * 
+                      rnorm(NPERM, cmaxi_mean, cmaxi_sds) * 
+                      WW * pvals[age] *
+                      sample(dietfVCHN, NPERM, replace = T),
+                    TCHN_cons_p1 = rnorm(NPERM, cmax_mean, cmax_sds) / 
+                      rnorm(NPERM, ref_cmax_mean,ref_cmax_sds) * 
+                      rnorm(NPERM, cmaxi_mean, cmaxi_sds) * 
+                      WW * 1 * #pval == 1
+                      sample(dietfVCHN, NPERM, replace = T))
+    
+    LWdata_f <- LWdata_f |>
+      #If the predicted value is below 0, fix to 0
+      dplyr::mutate(TCHN_cons_plt1 = ifelse(TCHN_cons_plt1 < 0, 0, TCHN_cons_plt1),
+                    TCHN_cons_p1 = ifelse(TCHN_cons_p1 < 0, 0, TCHN_cons_p1),
+                    # if the date is <= date of capture, set consumption to 0 as well
+                    TCHN_cons_plt1 = ifelse(Date <= date, 0, TCHN_cons_plt1),
+                    TCHN_cons_p1 = ifelse(Date <= date, 0, TCHN_cons_p1))
+    
+    
+    fish_list[[f]] <- LWdata_f
+  }
+  TCHNconsmatVA_plt1[[y]] <- fish_list 
+}
+stopCluster(cl = cluster)
