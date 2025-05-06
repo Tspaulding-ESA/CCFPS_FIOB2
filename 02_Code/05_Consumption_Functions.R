0603911f903be667fd5234efb47068eddc0a28c6
Taylor Spaulding <tspaulding@esassoc.com>
2025-05-06 07:48:27 -0700

    Build Lookup Tables for better processing

--- 02_Code/05_Consumption_Functions.R
+++ 02_Code/05_Consumption_Functions.R
@@ -6,6 +6,17 @@ CtempA1 <- read.csv(file.path("01_Data","Input","CtempA1.csv"))
 CtempA2 <- read.csv(file.path("01_Data","Input","CtempA2.csv"))
 CtempA3 <- read.csv(file.path("01_Data","Input","CtempA3.csv"))
 
+cparams <- data.frame(
+  ck1 = c(0.262, 0.255, rep(0.323, 8)),
+  ck4 = c(0.85, 0.9, rep(0.85, 8)),
+  ctl = c(30, 32, rep(30, 8)),
+  cto = c(19, 18, rep(15, 8)),
+  cq = c(6.6, 6.6, rep(7.4, 8)),
+  ctm = c(28, 29, rep(28, 8)))
+
+pvals <- c(rep(0.69, 2), rep(0.73, 1), rep(0.68, 1), rep(0.62, 6))
+refsize <- c(100, 350, rep(1000, 8))
+
 source(file.path("02_Code","Functions.R"))
 
 #consumption functions with variation
@@ -142,3 +153,62 @@ ggplot()+
 
 ggsave(file.path("01_Data","Output", "Figures", "CT_age_3.png"), device = "png", width = 6,
        height = 4, units = "in", dpi = 300)
+
+saveRDS(g1, file.path("01_Data","Input","g1.rds"))
+saveRDS(g2, file.path("01_Data","Input","g2.rds"))
+saveRDS(g3, file.path("01_Data","Input","g3.rds"))
+
+# Build Lookup Tables for faster processing
+# Predicted Cmax based on Age and Temp
+pred_cmax_lu <- list()
+for (i in 1:length(gs)){
+  pred_cmax_lu[[i]] <- predict(gs[i][[1]], newdata = data.frame("Temp" = TE),
+                               type = "response",se.fit = T) %>%
+    bind_cols("age" = i,
+              "TE" = TE)
+}
+pred_cmax_lu <- bind_rows(pred_cmax_lu) %>%
+  distinct()
+names(pred_cmax_lu) <- c("cmax_mean", "cmax_se.fit", "age", "TE")
+saveRDS(pred_cmax_lu, file.path("01_Data","Input","pred_cmax_lu.rds"))
+
+# Residuals of the Predicted Cmax relationship
+r_sq_lu <- list()
+for (i in 1:length(gs)){
+  r_sq_lu[[i]] <- sqrt(summary(gs[i][[1]])[10]$r.sq)
+}
+saveRDS(r_sq_lu, file.path("01_Data","Input","r_sq_lu.rds"))
+
+# Reference Cmax
+ref_cmax_lu <- list()
+for (i in 1:length(gs)){
+  ref_cmax_lu[[i]] <- xgxr::predict.nls(ConAllo,
+                                        newdata = data.frame("Wt" = refsize[i]), 
+                                        se.fit = TRUE, 
+                                        interval = "confidence", 
+                                        level = 0.995)$fit %>%
+    mutate("age" = i,
+           "ref_cmax_sds" = (fit-lwr)/1.96)
+}
+ref_cmax_lu <- bind_rows(ref_cmax_lu) %>%
+  distinct() %>%
+  select(age, "ref_cmax_mean" = fit, ref_cmax_sds)
+saveRDS(ref_cmax_lu, file.path("01_Data","Input","ref_cmax_lu.rds"))
+
+# Allometric Consumption Relationship
+WW = round(seq(0,max(LWdataA$upr)+100,0.01),2)
+
+con_allo_lu <- xgxr::predict.nls(ConAllo,
+                                 newdata = data.frame("Wt" = WW),
+                                 se.fit = TRUE, interval = "confidence",
+                                 level = 0.95)$fit
+
+con_allo_lu <- con_allo_lu %>%
+  bind_cols("WW" = WW) %>%
+  filter(!is.na(fit)) %>%
+  mutate(cmaxi_sds = (fit-lwr)/1.96) %>%
+  select(WW, "cmaxi_mean" = fit, cmaxi_sds)
+
+saveRDS(con_allo_lu,file.path("01_Data","Input","ConAlloLU.rds"))
+
+
